2e0486ca485a4f556a7f0e875484c02642adde98
lucak0909 <lucak0909@gmail.com>
2024-11-19 15:32:48 +0000

    Preparing for GUI as well as CLI implementation

---
+++ GUI.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pandas as pd
+import CLI  # Import your main script
